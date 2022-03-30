2ffe2f0bd69e439a4a1d247ee28b1ab326d69573
mazibaiteam <101470397+MaziBai-com@users.noreply.github.com>
2022-03-30 18:21:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+// CLIENT SIDE PAGES AND THE COMPONENTS 
+import "./App.css"
+// ADMIN SIDE PAGES AND THE COMPONENTS 
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminHome from './pages/Home/Home';
+import AdminMaid from './pages/maid/Maid'
+import AdminNewMaid from './pages/newMaid/NewMaid'
+import AdminMaidList from './pages/maidlist/MaidList'
+import AdminBookings from "./pages/allbookings/Bookings"
+import AdminAuth from "./pages/Auth/Auth"
+import AdminError from "./pages/ErrorPage"
+import AuthState from "./Context/AuthState"
+import AdminInstantBooking from "./pages/instantBooking/InstantBooking"
+function App() {
+  return (
+    <>
+    <AuthState>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/auth' element={<AdminAuth/>}/>
+                <Route path='/'  element={<AdminDashboard/>} >
+                      <Route path='/home' element={<AdminHome/>} />
+                      <Route path='/maid' element={<AdminMaid/>} />
+                      <Route path='/newmaid' element={<AdminNewMaid/>} />
+                      <Route path='/instantbooking' element={<AdminInstantBooking/>} />
+                      <Route path='/allmaids' element={<AdminMaidList/>} />
+                      <Route path='/bookings' element={<AdminBookings/>} />
+                </Route>
+                <Route path='*' element={<AdminError/>}/>
+            </Routes>
+        </BrowserRouter>  
+    </AuthState>
+    </>
+  );
+}
+
+export default App;
---
+++ src/urls.js
@@ -0,0 +1,9 @@
+// export const backendUrl = `https://mazibai.herokuapp.com/`
+export const backendUrl = `http://localhost:5000/`
+export const allBooknigs = `${backendUrl}api/bookings/allbookings`
+export const allUsersUrl  = `${backendUrl}api/auth/user/getall`
+export const confirmBookingUrl  = `${backendUrl}api/bookings/edit/`
+export  const addNotificationUrl = `${backendUrl}api/notifications/add/`
+export  const bookNowUrl = `${backendUrl}api/bookings/book`
+export const getallMaids = `${backendUrl}api/maid/getall`
+export const addMaidUrl = `${backendUrl}api/maid/register`
